c075cba11331e621a00cc22cad25de0dc2e6719f
Maksim Ekimovskii <ekimovsky.maksim@gmail.com>
2017-10-05 08:57:13 +0300

    Added producer. Troubleshooting

--- worker/app.py
+++ worker/app.py
@@ -2,13 +2,22 @@
 from aiokafka import AIOKafkaConsumer
 import asyncio
 
+from kafka import KafkaConsumer
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s %(levelname)s [%(name)s] %(message)s',
+)
 loop = asyncio.get_event_loop()
 
 
 async def consume():
+    # FIXME: it fails here
+    # https://github.com/aio-libs/aiokafka/blob/master/aiokafka/client.py#L446
     consumer = AIOKafkaConsumer(
-        'my_topic', 'my_other_topic',
-        loop=loop, bootstrap_servers='localhost:9092',
+        'my_topic1',
+        loop=loop, bootstrap_servers=['localhost:9092'],
         group_id="my-group")
 
     # Get cluster layout and join group `my-group`
@@ -24,5 +33,16 @@ async def consume():
         await consumer.stop()
 
 
+def consume_sync():
+    consumer = KafkaConsumer(
+        'my_topic',
+        bootstrap_servers=['localhost:9092'],
+        group_id="my-group")
+    for msg in consumer:
+        print("consumed: ", msg.topic, msg.partition, msg.offset,
+              msg.key, msg.value, msg.timestamp)
+
+
 if __name__ == '__main__':
-    loop.run_until_complete(consume())
+    # loop.run_until_complete(consume())
+    consume_sync()
